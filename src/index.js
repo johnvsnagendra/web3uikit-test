c73e7f670ddd0a083c961e9170c7250907dff4a5
John Versus <johnvsnagendra@users.noreply.github.com>
2022-02-20 23:13:04 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import Body from "./body";
 import Footer from "./footer";
 import { MoralisProvider } from "react-moralis";
 
-const appId = "zUwZT9PSLy02ZLe7lqyHn5MHPk6k9o7KV70AwWub";
-const serverUrl = "https://jnm2xedg402v.usemoralis.com:2053/server";
+const appId = "xxxx";
+const serverUrl = "xxx";
 
 ReactDOM.render(
   <React.StrictMode>
